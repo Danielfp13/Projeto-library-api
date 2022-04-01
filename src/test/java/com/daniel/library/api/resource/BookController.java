4a6cc9312885e0fb0fee687b17955b4fb52fb065
Daniel Fernando Pereira <daniel.si@outlook.com.br>
2022-03-31 22:21:13 -0300

    Criando teste na classe BookController no metodo createBookTest

---
+++ src/test/java/com/daniel/library/api/resource/BookController.java
@@ -0,0 +1,58 @@
+package com.daniel.library.api.resource;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest
+@AutoConfigureMockMvc
+public class BookController {
+
+    static String BOOK_API = "/api/books";
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    @DisplayName("Deve criar um Livro com sucesso.")
+    public void createBookTest() throws Exception {
+
+        String json = new ObjectMapper().writeValueAsString(null);
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(BOOK_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content("json");
+             mvc
+                .perform(request)
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect( jsonPath("id").isNotEmpty())
+                .andExpect( jsonPath("title").value("Meu Livro"))
+                .andExpect( jsonPath("author").value("Author"))
+                .andExpect( jsonPath("isbn").value("1234321"))
+        ;
+    }
+
+    @Test
+    @DisplayName("Deve lançar erro de valiação quando não houver dados suficientes.")
+    public void createInvalidBookTest() {
+
+    }
+}
