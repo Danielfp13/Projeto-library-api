830ac8eef25aa692d15b6f44fe37938af8192261
Daniel Fernando Pereira <daniel.si@outlook.com.br>
2022-04-06 00:07:43 -0300

    Logs, Métricas e Monitoramento da API com Actuator e Spring Boot Admin

--- src/main/java/com/daniel/library/api/BookController.java
+++ src/main/java/com/daniel/library/api/BookController.java
@@ -4,6 +4,7 @@ import com.daniel.library.model.dto.BookDTO;
 import com.daniel.library.model.entity.Book;
 import com.daniel.library.model.service.BookService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -20,6 +21,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/books")
 @AllArgsConstructor
+@Slf4j
 public class BookController {
 
     private BookService service;
@@ -30,23 +32,27 @@ public class BookController {
         Book entity = modelMapper.map(dto, Book.class);
         entity = service.save(entity);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(entity.getId()).toUri();
+        log.info("Criando um livro para no endpoint: {}", uri);
         return ResponseEntity.created(uri).body(modelMapper.map(entity, BookDTO.class));
     }
 
     @GetMapping("/{id}")
     ResponseEntity<BookDTO> findById(@PathVariable Long id) {
+        log.info("Obtendo um livro com identificador: {}", id);
         BookDTO bookDTO = modelMapper.map(service.findById(id), BookDTO.class);
         return ResponseEntity.ok().body(bookDTO);
     }
 
     @DeleteMapping("/{id}")
     ResponseEntity<Void> delete(@PathVariable Long id) {
+        log.info("Deletando um livro com identificador: {}", id);
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/{id}")
     ResponseEntity<BookDTO> updateController(@PathVariable Long id, @RequestBody @Valid BookDTO bookDTO) {
+        log.info("Alterando informações do livro com identificador: {}", id);
         bookDTO = service.update(id, bookDTO);
         return ResponseEntity.ok().body(bookDTO);
     }
