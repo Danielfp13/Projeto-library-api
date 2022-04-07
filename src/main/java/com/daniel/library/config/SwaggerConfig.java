5f656678fe8ca5e7ebb6225f6f023e8938b1395a
Daniel Fernando Pereira <daniel.si@outlook.com.br>
2022-04-07 19:32:09 -0300

    Add test main

--- src/main/java/com/daniel/library/config/SwaggerConfig.java
+++ src/main/java/com/daniel/library/config/SwaggerConfig.java
@@ -28,7 +28,6 @@ import java.util.List;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
--- src/test/java/com/daniel/library/LibraryApplicationTests.java
+++ src/test/java/com/daniel/library/LibraryApplicationTests.java
@@ -1,11 +1,12 @@
 package com.daniel.library;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LibraryApplicationTests {
-
+    @Test
+    void main() {
+        LibraryApplication.main(new String[]{});
+    }
 }
