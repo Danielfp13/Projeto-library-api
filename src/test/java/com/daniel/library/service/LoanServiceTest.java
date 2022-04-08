0f611e8f2f0f7a2fac100a209ca3ff3e38017482
Daniel Fernando Pereira <daniel.si@outlook.com.br>
2022-04-08 17:24:25 -0300

    Teste de buscar emprestimo por id

--- src/test/java/com/daniel/library/service/LoanServiceTest.java
+++ src/test/java/com/daniel/library/service/LoanServiceTest.java
@@ -5,35 +5,37 @@ import com.daniel.library.model.entity.Loan;
 import com.daniel.library.model.repository.LoanRepository;
 import com.daniel.library.model.service.LoanService;
 import com.daniel.library.model.service.exceptions.BusinessException;
+import com.daniel.library.model.service.exceptions.ObjectNotFondException;
 import com.daniel.library.model.service.impl.LoanServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 public class LoanServiceTest {
 
-    LoanService service;
+    LoanService loanService;
 
     @MockBean
-    LoanRepository repository;
+    LoanRepository loanRepository;
 
     @BeforeEach
     public void setUp() {
-        this.service = new LoanServiceImpl(repository);
+        this.loanService = new LoanServiceImpl(loanRepository);
     }
 
     @Test
@@ -48,10 +50,10 @@ public class LoanServiceTest {
         Loan savedLoan = new Loan(1L, customer, "fulano@email.com", book, LocalDate.now(), false);
 
 
-        when(repository.existsByBookAndNotReturned(book)).thenReturn(false);
-        when(repository.save(savingLoan)).thenReturn(savedLoan);
+        when(loanRepository.existsByBookAndNotReturned(book)).thenReturn(false);
+        when(loanRepository.save(savingLoan)).thenReturn(savedLoan);
 
-        Loan loan = service.save(savingLoan);
+        Loan loan = loanService.save(savingLoan);
 
         assertThat(loan.getId()).isEqualTo(savedLoan.getId());
         assertThat(loan.getBook().getId()).isEqualTo(savedLoan.getBook().getId());
@@ -61,22 +63,66 @@ public class LoanServiceTest {
 
     @Test
     @DisplayName("Deve lançar erro de negócio ao salvar um empréstimo com livro já emprestado")
-    public void loanedBookSaveTest(){
+    public void loanedBookSaveTest() {
         Book book = new Book(1L, "Ana", "O Código", "123");
         String customer = "Fulano";
 
         Loan savingLoan = new Loan(1L, customer, "fulano@email.com", book, LocalDate.now(), false);
 
-        when(repository.existsByBookAndNotReturned(book)).thenReturn(true);
+        when(loanRepository.existsByBookAndNotReturned(book)).thenReturn(true);
 
-        Throwable exception = Assertions.catchThrowable(() -> service.save(savingLoan));
+        Throwable exception = Assertions.catchThrowable(() -> loanService.save(savingLoan));
 
         assertThat(exception)
                 .isInstanceOf(BusinessException.class)
                 .hasMessage("Livro já emprestado.");
 
-        Mockito.verify(repository, Mockito.never()).save(savingLoan);
+        Mockito.verify(loanRepository, Mockito.never()).save(savingLoan);
 
+    }
+
+    @Test
+    @DisplayName("Deve retornar notfound tentar um emprestimo por id quando ele não existe na base.")
+    public void loanNotFoundByIdTest() {
+        Long id = 1L;
+
+        org.junit.jupiter.api.Assertions.assertThrows(ObjectNotFondException.class,
+                () -> loanService.findById(id));
+        try {
+            loanService.findById(id);
+        } catch (Exception e) {
+            assertThat(ObjectNotFondException.class).isEqualTo(e.getClass());
+            assertThat("Não existe emprestimo com esse id.").isEqualTo(e.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName(" Deve obter as informações de um empréstimo pelo ID")
+    public void getLoanDetaisTest(){
+        //cenário
+        Long id = 1l;
+
+        Loan loan = createLoan();
+        loan.setId(id);
+
+        Mockito.when( loanRepository.findById(id) ).thenReturn(Optional.of(loan));
+
+        //execucao
+        Loan result = loanService.findById(id);
+
+        //verificacao
+        assertThat(result).isNotNull();
+        assertThat(result.getId()).isEqualTo(id);
+        assertThat(result.getCustomer()).isEqualTo(loan.getCustomer());
+        assertThat(result.getBook()).isEqualTo(loan.getBook());
+        assertThat(result.getLoanDate()).isEqualTo(loan.getLoanDate());
+
+        Mockito.verify( loanRepository ).findById(id);
+
+    }
+
+    private Loan createLoan() {
+        return  new Loan(1L, "Fulano","fulano@email.com", null, LocalDate.now(),false);
     }
 
 }
