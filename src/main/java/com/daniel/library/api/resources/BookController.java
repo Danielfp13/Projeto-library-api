5230ea52a19b817f7bcdc6935f000c65ed003afb
Daniel Fernando Pereira <daniel.si@outlook.com.br>
2022-04-09 13:47:36 -0300

    Consulta de emprestimo atrazado

--- src/main/java/com/daniel/library/api/resources/BookController.java
+++ src/main/java/com/daniel/library/api/resources/BookController.java
@@ -1,8 +1,11 @@
 package com.daniel.library.api.resources;
 
 import com.daniel.library.model.dto.BookDTO;
+import com.daniel.library.model.dto.LoanDTO;
 import com.daniel.library.model.entity.Book;
+import com.daniel.library.model.entity.Loan;
 import com.daniel.library.model.service.BookService;
+import com.daniel.library.model.service.LoanService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
@@ -27,14 +30,15 @@ import java.util.stream.Collectors;
 @Api("API Biblioteca.")
 public class BookController {
 
-    private BookService service;
+    private BookService bookService;
+    private LoanService loanService;
     private ModelMapper modelMapper;
 
     @PostMapping
     @ApiOperation("Salvar livro.")
     public ResponseEntity<BookDTO> create(@RequestBody @Valid BookDTO dto) {
         Book entity = modelMapper.map(dto, Book.class);
-        entity = service.save(entity);
+        entity = bookService.save(entity);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(entity.getId()).toUri();
         log.info("Criando um livro para no endpoint: {}", uri);
         return ResponseEntity.created(uri).body(modelMapper.map(entity, BookDTO.class));
@@ -44,7 +48,7 @@ public class BookController {
     @ApiOperation("Buscar livro por ID.")
     ResponseEntity<BookDTO> findById(@PathVariable Long id) {
         log.info("Obtendo um livro com identificador: {}", id);
-        BookDTO bookDTO = modelMapper.map(service.findById(id), BookDTO.class);
+        BookDTO bookDTO = modelMapper.map(bookService.findById(id), BookDTO.class);
         return ResponseEntity.ok().body(bookDTO);
     }
 
@@ -52,7 +56,7 @@ public class BookController {
     @ApiOperation("Excluir livro.")
     ResponseEntity<Void> delete(@PathVariable Long id) {
         log.info("Deletando um livro com identificador: {}", id);
-        service.delete(id);
+        bookService.delete(id);
         return ResponseEntity.noContent().build();
     }
 
@@ -60,7 +64,7 @@ public class BookController {
     @ApiOperation("Alterar livro.")
     ResponseEntity<BookDTO> update(@PathVariable Long id, @RequestBody @Valid BookDTO bookDTO) {
         log.info("Alterando informações do livro com identificador: {}", id);
-        bookDTO = service.update(id, bookDTO);
+        bookDTO = bookService.update(id, bookDTO);
         return ResponseEntity.ok().body(bookDTO);
     }
 
@@ -68,12 +72,29 @@ public class BookController {
     @ApiOperation("Busca paginada com parâmetros.")
     public Page<BookDTO> find(BookDTO dto, Pageable pageRequest) {
         Book filter = modelMapper.map(dto, Book.class);
-        Page<Book> result = service.find(filter, pageRequest);
+        Page<Book> result = bookService.find(filter, pageRequest);
         List<BookDTO> list = result.getContent()
                 .stream()
                 .map(entity -> modelMapper.map(entity, BookDTO.class))
                 .collect(Collectors.toList());
 
         return new PageImpl<>(list, pageRequest, result.getTotalElements());
+    }
+
+    @GetMapping("{id}/loans")
+    @ApiOperation("Busca paginada de livro emprestados.")
+    public Page<LoanDTO> loansByBook(@PathVariable Long id, Pageable pageable) {
+        Book book = bookService.findById(id);
+        Page<Loan> result = loanService.findLoansByBook(book, pageable);
+        List<LoanDTO> list = result.getContent()
+                .stream()
+                .map(loan -> {
+                    Book loanBook = loan.getBook();
+                    BookDTO bookDTO = modelMapper.map(loanBook, BookDTO.class);
+                    LoanDTO loanDTO = modelMapper.map(loan, LoanDTO.class);
+                    loanDTO.setBook(bookDTO);
+                    return loanDTO;
+                }).collect(Collectors.toList());
+        return new PageImpl<LoanDTO>(list, pageable, result.getTotalElements());
     }
 }
--- src/main/java/com/daniel/library/model/entity/Book.java
+++ src/main/java/com/daniel/library/model/entity/Book.java
@@ -2,15 +2,13 @@ package com.daniel.library.model.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 public class Book {
 
@@ -21,5 +19,15 @@ public class Book {
     private String author;
     private String title;
     private String isbn;
+
+    @OneToMany(mappedBy = "book", fetch = FetchType.LAZY)
+    List<Loan> loans = new ArrayList<>();
+
+    public Book(Long id, String author, String title, String isbn) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.isbn = isbn;
+    }
 }
 
